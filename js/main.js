e7008aa0dd16d7254980844ebdbb0152967df214
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2026-02-10 16:04:19 -0600

    Add main.js for system online messages

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/* js/main.js */
+console.log("%c SYSTEM ONLINE ", "background: #58a6ff; color: #000; padding: 4px; font-weight: bold; border-radius: 4px;");
+console.log("%c Welcome to Alenia Studios Terminals.", "color: #8b949e; font-family: monospace;");
+console.log("K.A.I.A. is watching...");
+
+// Aquí podrás agregar animaciones futuras o interacciones
